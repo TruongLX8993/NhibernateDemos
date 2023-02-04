b9d1c72b35018ceae126af901676984659217541
truonglx <truonglx8993@gmail.com>
2023-02-04 15:56:01 +0700

    Update Program.cs

--- MappingWithClassProperty/Program.cs
+++ MappingWithClassProperty/Program.cs
@@ -8,7 +8,7 @@ public static class Program
     public static void Main(string[] args)
     {
         var connectionString =
-            "data source=103.74.123.8;initial catalog=rwtfgrxz_motor;user id=rwtfgrxz_sa;password=Motor@123;MultipleActiveResultSets=True;";
+            "";
         var nhibernateSessionFactory = new NhibernateSessionFactory(connectionString, typeof(Program).Assembly);
         var session = nhibernateSessionFactory.Create();
         var res = session.Query<Customer>()
